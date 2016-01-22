96be503d336db6d784f9e35788e8eaec92bb0e8f
alibaevafarida <87711631643@mail.ru>
2016-01-22 12:50:33 +0600

    added console app 2

---
+++ ConsoleApplication2/ConsoleApplication2/primenumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numb;
+            numb = Console.ReadLine();
+            string[] data = numb.Split();
+            foreach (string a in data)
+            {
+                int cnt = 0;
+                for(int i=1; i<Convert.ToInt16(a); i++) {
+                    if (Convert.ToInt32(a) % i == 0)
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt <= 1)
+                {
+                    Console.WriteLine(a);
+                }
+           }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
