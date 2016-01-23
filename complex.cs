7f3e3f345e3955566ca85794f67d4e33a3f44012
alibaevafarida <87711631643@mail.ru>
2016-01-23 10:28:19 +0600

    added complex

---
+++ complex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Complex
+    {
+        public int a, b;
+
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3 = new Complex(c1.a + c2.a, c1.b + c2.b);
+            return c3;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c = new Complex(3, 6);
+            Complex d = new Complex(2, 3);
+            Complex w = c + d;
+            Console.WriteLine(w);
+            Console.ReadKey();
+        }
+    }
+}
