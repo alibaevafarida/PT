78feed18266e0b74c4b3e70ea1809c33ac1d2096
alibaevafarida <87711631643@mail.ru>
2016-01-26 00:34:48 +0600

    added new field and new method

--- class Student.cs
+++ class Student.cs
@@ -10,29 +10,41 @@ namespace ConsoleApplication4
     {
         public string Name;
         public int Age;
-        public double GPA;
-        public string ID;
+        private double GPA;
+        private string ID;
+        private bool Grant;
 
-        public Student(string Name, int Age, double GPA, string ID)
+        public Student(string Name, int Age, double GPA, string ID, bool Grant)
         {
             this.Name = Name;
             this.Age = Age;
             this.GPA = GPA;
             this.ID = ID;
+            this.Grant = Grant;
         }
         public override string ToString()
         {
             return this.Name + "\n " + ID +" \n " + Age + "years old \n " + "GPA is " + GPA;
+        }
+        public void  IsGrant()
+        {
+            if (Grant == true)
+                Console.WriteLine(this.Name + " has Grant \n ");
+            else
+                Console.WriteLine(this.Name + " has no Grant \n");
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Student newSt1 = new Student("Arlen Maratov" , 18 , 3.9 , "15BD02055");
-            Student newSt2 = new Student ("Nurai Kim" , 17 , 4, "15BD02056" );
+            Student newSt1 = new Student("Arlen Maratov" , 18 , 3.9 , "15BD02055" , false);
+            Student newSt2 = new Student ("Nurai Kim" , 17 , 4, "15BD02056" , true );
             Console.WriteLine(newSt1.ToString());
+            newSt1.IsGrant();
             Console.WriteLine(newSt2.ToString());
+            newSt2.IsGrant();
+
             Console.ReadKey();
 
         }
