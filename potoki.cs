002a28fbb64caf8dff6b054dccc00919cb534af3
alibaevafarida <87711631643@mail.ru>
2016-01-29 03:53:31 +0600

    added the filestream

---
+++ potoki.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            FileStream file1 = new FileStream("file1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite); // создаем поток файлов
+            StreamWriter sw = new StreamWriter(file1); //создаем потоковый писатель
+            for (int i = 1; i <= 10; i++)//исываем при помощи цикла цифры от одного до десяти
+            {
+              sw.Write(i + ",");
+            };
+            StreamReader sr=new StreamReader(file1); 
+            Console.WriteLine( sr.ReadToEnd()); //читать  до конца
+            sw.Close(); 
+           sr.Close(); // закрываем потоки 
+            Console.ReadKey();
+
+            
+
+        
+        }
+        
+
+
+
+    }
+}
