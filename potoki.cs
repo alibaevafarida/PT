248da3403df4710673abf0239e189c529bf1c70c
alibaevafarida <87711631643@mail.ru>
2016-01-29 11:46:18 +0600

    added min, max

--- potoki.cs
+++ potoki.cs
@@ -13,16 +13,45 @@ namespace ConsoleApplication8
         {
             
             FileStream file1 = new FileStream("file1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite); // создаем поток файлов
-            StreamWriter sw = new StreamWriter(file1); //создаем потоковый писатель
-            for (int i = 1; i <= 10; i++)//исываем при помощи цикла цифры от одного до десяти
+            FileStream file2 = new FileStream("file2.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(file2); //создаем потоковый писатель
+             StreamReader sr=new StreamReader(file1);
+
+            /* for (int i = 1; i <= 10; i++)//исываем при помощи цикла цифры от одного до десяти
             {
               sw.Write(i + ",");
             };
-            StreamReader sr=new StreamReader(file1); 
-            Console.WriteLine( sr.ReadToEnd()); //читать  до конца
+             */ 
+
+            string[] numb = sr.ReadLine().Split(',');
+            int min = Convert.ToInt32(numb[0]);
+            int max = Convert.ToInt32(numb[0]);
+            
+            foreach (string s in numb)
+            {
+                
+
+                if (int.Parse(s) < min)
+                {
+                    min = int.Parse(s);
+                }
+                if (int.Parse(s) > max)
+                {
+                    max = int.Parse(s);
+                }
+            }
+
+            Console.WriteLine("minimum = {0} , maximum = {1}" ,min, max);
+
+
+           
             sw.Close(); 
-           sr.Close(); // закрываем потоки 
+           sr.Close();
+           file1.Close();
+           file2.Close();// закрываем потоки 
             Console.ReadKey();
+
+
 
             
 
