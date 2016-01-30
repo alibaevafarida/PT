19ade3930d5af1cb286dbbc23b3463c2467af327
alibaevafarida <87711631643@mail.ru>
2016-01-30 13:59:13 +0600

    all labs are sorted

---
+++ Week1/ConsoleApplication2/primenumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numb; //создаем строку в которой записаны числа(через пробел)
+            numb = Console.ReadLine();// считываем с консоли эту строку
+            string[] data = numb.Split(); // создаем отдельный массив data в котором сохраняем числа как элементы(отделяя их Split()
+            foreach (string a in data) // по циклу каждый элемент массива data записывается как а;
+            {
+                int cnt = 0;  // при каждом новом элементе счетчик = 0
+                for(int i=1; i<=Convert.ToInt16(a); i++) { // создаем цикл от 1 до самого числа а (конвертируем в инт потому что он изначально явл строкой
+                    if (Convert.ToInt32(a) % i == 0) // если а кратно 1 и себе
+                    {
+                        cnt++; // счетчик прибавляется
+                    }
+                }
+                if (cnt == 2) // если а простое 
+                {
+                    Console.WriteLine(a); // выводим на экран а
+                }
+           }
+            Console.ReadKey(); // позволяет видеть наш вывод 
+
+
+
+        }
+    }
+}
---
+++ Week2/min_prime/ConsoleApplication2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+           
+            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream fr = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+            string[] numb = sr.ReadLine().Split();
+            List<int> prime = new List<int>();
+
+                int cnt = 0;
+                for (int i = 0; i < numb.Length; i++)
+                {
+                    cnt = 0;
+                    int a= int.Parse(numb[i]);
+                    for(int j=1; j<=a; j++) {
+                         if (a % j == 0)
+                         {
+                             cnt++;
+                          }
+                    }
+                    if (cnt == 2)
+                    {
+                        prime.Add(a);
+                    }
+                }
+                    int min = prime[0];
+                    foreach (int k in prime)
+                    {
+                        if (k < min)
+                        {
+                            min = k;
+                        }
+                    }
+                    StreamWriter sw = new StreamWriter(fr);
+                    sw.WriteLine(min);
+                    sw.Close();
+                    fs.Close();
+                    fr.Close();
+                    sr.Close();
+                    Console.WriteLine("the minimum is {0}", min);
+               Console.ReadKey();
+            }
+           
+        }
+
+    
+}
