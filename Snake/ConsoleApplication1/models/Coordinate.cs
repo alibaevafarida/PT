68641f87fd98b45400f56282c3062a26cefc7e43
alibaevafarida <87711631643@mail.ru>
2016-02-03 10:10:46 +0600

    added snakegame

---
+++ Snake/ConsoleApplication1/models/Coordinate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.models
+{
+    class Coordinate  
+    {
+        
+        public int x, y; // we create two points of coordination 
+        public Coordinate (int x, int y) // constructor 
+        {
+            this.x = x;      // ukazateli 
+            this.y = y;
+        }
+    }
+
+    
+}
---
+++ Snake/ConsoleApplication1/models/Drawer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System. IO;
+using System.Xml.Serialization; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.models
+{
+    public class Drawer
+    {
+        public ConsoleColor color; 
+        public char figure;
+        public List<Coordinate> body = new List<Coordinate>(); //vector coordinat
+        public  Drawer () { } //?
+        public  void Draw { //function to draw
+            Console.ForegroundColor=color;   // 
+            foreach (Coordinate  c in body) //each point became figure
+                {
+                Console.SetCursorPosition(p.x, p.y); 
+                Console.Write(figure);
+                }
+        }
+        
+    }
+}
+        }
+
+    }
+}
