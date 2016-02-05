4ab2b1cb6383eb893186961a1d11b394f14c4079
alibaevafarida <87711631643@mail.ru>
2016-02-05 11:47:33 +0600

    added 1 task in lab2

---
+++ Week2/DirectoryTree_stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryTree(@"C:\Users\Farida\Desktop\PT_labs\testfolder");
+
+
+        }
+        static void DirectoryTree(string path)
+        {
+            Stack<string> st = new Stack<string>();
+            Console.WriteLine("In folder with path: " + path  + " there are " + Directory.GetFiles(path).Length + "files");
+            st.Push(path);
+
+            while (st.Count > 0)
+            {
+                string[] subfolders = Directory.GetDirectories(st.Pop());
+                foreach (string s in subfolders)
+                {
+                    Console.WriteLine("In Folder with path :" + s + " there is" + Directory.GetFiles(s).Length + " file");
+                    st.Push(s);
+                    Console.ReadKey();
+                }
+            }
+        }
+    
+   }
+}
